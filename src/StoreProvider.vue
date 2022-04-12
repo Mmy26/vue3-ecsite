fb9d87de7a251d9d879f0650e7cabbf60fa5f903
rikoysd <riko.yoshida@rakus-partners.co.jp>
2022-04-12 15:58:35 +0900

    provide設定

--- src/StoreProvider.vue
+++ src/StoreProvider.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts">
 import { provide } from "vue";
 import { itemListKey, useItemList } from "@/providers/useItemListProvider";
+import { orderProviderKey, useOrderStore } from "./providers/useOrderProvider";
 
 provide(itemListKey, useItemList());
+provide(orderProviderKey, useOrderStore());
 </script>
 
 <template>
