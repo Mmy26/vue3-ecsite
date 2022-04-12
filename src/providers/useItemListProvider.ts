45363d097f269a905ac73fd563237a1a3e140ccc
grandorder999 <suzuo.kawabata@rakus-partners.co.jp>
2022-04-12 11:46:40 +0900

    検索のactionを追加

--- src/providers/useItemListProvider.ts
+++ src/providers/useItemListProvider.ts
@@ -4,19 +4,30 @@ import { reactive, toRefs, type InjectionKey } from "vue";
 
 type State = {
   itemList: Array<Item>;
+  searchItemName: string;
 };
 
 export const useItemList = () => {
   //state
   const globalState = reactive<State>({
     itemList: [],
+    searchItemName: "",
   });
 
   //actions
   const setItemList = async (): Promise<void> => {
     const response = await axios.get<Array<Item>>("urlを入れる");
   };
-  return { ...toRefs(globalState), setItemList };
+  const searchItemList = () => {
+    return (name: string) => {
+      return globalState.itemList.filter(
+        (item) =>
+          item.name.includes(name.toUpperCase()) || item.name.includes(name)
+      );
+    };
+  };
+
+  return { ...toRefs(globalState), setItemList, searchItemList };
 };
 
 type storeType = ReturnType<typeof useItemList>;
