8deb97023774425101f309b3903079aa4a131c04
Mmy26 <yu.b1994masamura@gmail.com>
2022-04-12 14:36:31 +0900

    コンフリクトの修正

--- src/App.vue
+++ src/App.vue
@@ -5,32 +5,8 @@ import Footer from "./components/Footer.vue";
 </script>
 
 <template>
-<<<<<<< HEAD
-  <header>
-    <el-button>I am ElButton</el-button>
-    <img
-      alt="Vue logo"
-      class="logo"
-      src="@/assets/logo.svg"
-      width="125"
-      height="125"
-    />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-        <RouterLink to="/itemDetail">itemDetail</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-=======
   <Header />
-  <!-- ここにStoreProvider.vueを挿入 -->
->>>>>>> origin/develop
+  <!-- ここにStoreProvider.vueを挿入 -->s
   <RouterView />
   <!--  -->
   <Footer />
