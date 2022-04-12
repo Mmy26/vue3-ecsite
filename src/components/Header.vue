1bbad9157e4a2b6adfd1d58bf9cfcee35f9d62ea
ryota-09 <n.ryo950911@gmail.com>
2022-04-12 11:32:12 +0900

    headerの設置

---
+++ src/components/Header.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import "element-plus/theme-chalk/display.css";
+
+
+</script>
+
+<template>
+   <el-header>
+    <el-row class="row-bg" justify="space-between">
+      <el-col :span="6"
+        ><div class="grid-content bg-purple">Title</div></el-col
+      >
+      <el-col :span="6"
+        ><div class="grid-content bg-purple">
+          <el-space :size="10">
+            <span>MyPage</span>
+            <span>About</span>
+            <span>Contact</span>
+          </el-space>
+        </div></el-col
+      >
+    </el-row>
+  </el-header>
+</template>
+<style scoped>
+.row-bg{
+  background-color: #d3dce6;
+  height: 5vh;
+}
+
+</style>
