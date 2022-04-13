0eec874e205c083772b3a664e869791164412ed4
ryota-09 <n.ryo950911@gmail.com>
2022-04-13 09:33:34 +0900

    リンクの追加

--- components.d.ts
+++ components.d.ts
@@ -10,6 +10,7 @@ declare module '@vue/runtime-core' {
     ElFormItem: typeof import('element-plus/es')['ElFormItem']
     ElHeader: typeof import('element-plus/es')['ElHeader']
     ElInput: typeof import('element-plus/es')['ElInput']
+    ElLink: typeof import('element-plus/es')['ElLink']
     ElRow: typeof import('element-plus/es')['ElRow']
     ElSpace: typeof import('element-plus/es')['ElSpace']
     Footer: typeof import('./src/components/Footer.vue')['default']
