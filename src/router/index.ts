c3457988f9e65e7cafd5e1eeea5dba9d6007089d
Mmy26 <yu.b1994masamura@gmail.com>
2022-04-14 12:05:37 +0900

    APIから情報取得のメソッド

--- src/router/index.ts
+++ src/router/index.ts
@@ -8,7 +8,7 @@ const router = createRouter({
       path: "/",
       name: "home",
       redirect: () => {
-        return "itemList"
+        return "itemList";
       },
       component: HomeView,
     },
@@ -21,16 +21,16 @@ const router = createRouter({
       component: () => import("../views/AboutView.vue"),
     },
     {
-      path: '/orderConfirm',
-      component: () => import('../views/OrderConfirm.vue')
+      path: "/orderConfirm",
+      component: () => import("../views/OrderConfirm.vue"),
     },
     {
-      path: '/registerUser',
-      name: 'RegisterUser',
+      path: "/registerUser",
+      name: "RegisterUser",
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/RegisterUser.vue')
+      component: () => import("../views/RegisterUser.vue"),
     },
     {
       path: "/itemList",
@@ -49,6 +49,10 @@ const router = createRouter({
       path: "/login",
       name: "Login",
       component: () => import("../views/Login.vue"),
+    },
+    {
+      path: "/orderHistory",
+      component: () => import("../views/OrderHistory.vue"),
     },
   ],
 });
--- src/views/OrderHistory.vue
+++ src/views/OrderHistory.vue
@@ -1,5 +1,42 @@
 <script setup lang="ts">
+import { Order } from "@/types/Order";
+import { User } from "@/types/User";
 import { ref } from "vue";
+import axios from "axios";
+// APIから受け取った情報を入れるインスタンス
+const currentOrderList = ref<Order[]>([]);
+// ログイン中のユーザーー情報（ストアから取得）
+const currentUser = ref<User>(new User(0, "", "", "", "", "", ""));
+
+// APIから情報取得
+const getOrderData = async (): Promise<void> => {
+  console.log("メソッド起動");
+  const response = await axios.get(
+    `"http://153.127.48.168:8080/ecsite-api/item/items/noodle"/1`
+  );
+  console.dir(JSON.stringify(response.data));
+  const orders = ref(response.data.orders);
+  for (const order of orders) {
+    currentOrderList.value.push(
+      new Order(
+        order.id,
+        order.userId,
+        order.status,
+        order.totalPrice,
+        order.orderDate,
+        order.destinationName,
+        order.destinationEmail,
+        order.destinationZipcode,
+        order.destinationAddress,
+        order.destinationTel,
+        order.deliveryTime,
+        order.paymentMethod,
+        currentUser.value as User,
+        order.orderItemList
+      )
+    );
+  }
+};
 </script>
 <template></template>
 <style scoped></style>
