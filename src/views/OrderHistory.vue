4326601fe3f6975cc095d681c497264242f7aa47
Mmy26 <yu.b1994masamura@gmail.com>
2022-04-14 15:40:46 +0900

    ストアからログイン中のユーザー情報の取得

--- src/views/OrderHistory.vue
+++ src/views/OrderHistory.vue
@@ -1,18 +1,24 @@
 <script setup lang="ts">
 import { Order } from "@/types/Order";
-import { User } from "@/types/User";
-import { ref } from "vue";
+import type { User } from "@/types/User";
+import { inject, ref } from "vue";
 import axios from "axios";
+import { useUserProviderKey } from "@/providers/useUserProvider";
 // APIから受け取った情報を入れるインスタンス
 const currentOrderList = ref<Order[]>([]);
 // ログイン中のユーザーー情報（ストアから取得）
-const currentUser = ref<User>(new User(0, "", "", "", "", "", ""));
+const userStore = inject(useUserProviderKey);
+
+// キーのundefined の時のエラー回避処理
+if (!userStore) {
+  throw new Error("Error");
+}
 
 // APIから情報取得
 const getOrderData = async (): Promise<void> => {
   console.log("メソッド起動");
   const response = await axios.get(
-    `"http://153.127.48.168:8080/ecsite-api/item/items/noodle"/1`
+    `"http://153.127.48.168:8080/ecsite-api/item/items/noodle"/${userStore.currentUser.value.id}`
   );
   console.dir(JSON.stringify(response.data));
   const orders = ref(response.data.orders);
@@ -31,12 +37,23 @@ const getOrderData = async (): Promise<void> => {
         order.destinationTel,
         order.deliveryTime,
         order.paymentMethod,
-        currentUser.value as User,
+        userStore.currentUser.value as User,
         order.orderItemList
       )
     );
   }
 };
 </script>
-<template></template>
+<template>
+  <h1>注文履歴</h1>
+  <div v-for="currentUserData of currentOrderList">
+    <span>合計金額:{{ currentUserData.calcTotalPrice }}</span>
+    <div v-for="orderItem of currentUserData.orderItemList">
+      <span>name:{{ orderItem.item.name }}</span>
+      <span>size:{{ orderItem.size }}</span>
+      <span>quantity:{{ orderItem.quantity }}</span>
+      <span>image</span><img :src="orderItem.item.imagePath" />
+    </div>
+  </div>
+</template>
 <style scoped></style>
