5b1b580a16f35f40f9306b4e20f6d1464770fddb
rikoysd <riko.yoshida@rakus-partners.co.jp>
2022-04-15 16:13:03 +0900

    注文完了ページCSS調整

--- src/views/OrderFinished.vue
+++ src/views/OrderFinished.vue
@@ -1,9 +1,72 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useRouter } from "vue-router";
+
+const router = useRouter();
+
+const backToItemList = () => {
+  router.push("/itemList");
+};
+
+const orderHistory = () => {
+  router.push("/orderHistory");
+};
+</script>
 
 <template>
-  <h2>ご注文ありがとうございました。</h2>
-  <router-link to="/itemList">商品一覧に戻る</router-link> <br />
-  <router-link to="/orderHistory">注文履歴を見る</router-link>
+  <div class="back-img">
+    <div class="img-position">
+      <img src="/img_noodle/background.png" />
+    </div>
+    <div class="msg">
+      <h2>ご注文ありがとうございました。</h2>
+      <p>またのご注文をお待ちしております。</p>
+      <div class="button">
+        <div>
+          <el-button type="danger" plain @click="backToItemList"
+            >商品一覧に戻る</el-button
+          >
+          <el-button type="danger" plain @click="orderHistory"
+            >注文履歴を見る</el-button
+          >
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.back-img {
+  position: relative;
+}
+
+.msg {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+
+img {
+  margin-top: 50px;
+  width: 600px;
+  text-align: center;
+}
+
+.img-position{
+  text-align: center;
+}
+
+h2 {
+  text-align: center;
+}
+
+p {
+  text-align: center;
+  margin-bottom: 30px;
+}
+
+.button {
+  display: flex;
+  justify-content: center;
+}
+</style>
