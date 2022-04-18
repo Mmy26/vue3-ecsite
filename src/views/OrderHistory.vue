56aac183c30bc882b9e18002ecb453d98e67c311
Mmy26 <yu.b1994masamura@gmail.com>
2022-04-18 12:01:58 +0900

    表示の修正

--- src/views/OrderHistory.vue
+++ src/views/OrderHistory.vue
@@ -3,6 +3,8 @@ import { Order } from "@/types/Order";
 import type { User } from "@/types/User";
 import { inject, onMounted, ref } from "vue";
 import axios from "axios";
+import { ElNotification } from "element-plus";
+import { RouterLink } from "vue-router";
 import { useUserProviderKey } from "@/providers/useUserProvider";
 import router from "@/router";
 // APIから受け取った情報を入れるインスタンス
@@ -18,6 +20,11 @@ console.log(userStore.currentUser.value.email);
 
 // ログインしていなければ、ログイン画面に遷移
 if (userStore.currentUser.value.email === "") {
+  ElNotification({
+    title: "Error",
+    message: "ログインしてください",
+    type: "error",
+  });
   router.push("/login");
 }
 console.log(userStore.currentUser.value.id);
@@ -51,16 +58,21 @@ const getOrderData = async (): Promise<void> => {
       )
     );
   }
+  //オブジェクトの昇順ソート
+  currentOrderList.value.sort(function (a, b) {
+    return b.orderDate < a.orderDate ? -1 : 1;
+  });
 };
+
 // 非同期で上記のメソッドを実行
 onMounted(getOrderData);
 </script>
 <template>
-  <h1>注文履歴</h1>
-  <div v-if="currentOrderList.length >= 1">
+  <h1><img class="icon" :src="'/img_noodle/9654.png'" />注文履歴</h1>
+  <div class="container" v-if="currentOrderList.length >= 1">
     <el-row :gutter="20">
       <el-col :span="1"> <!-- ここは余白--> </el-col>
-      <el-col :span="2"><div class="grid-content bg-purple">番号</div></el-col>
+
       <el-col :span="3"
         ><div class="grid-content bg-purple">商品画像</div></el-col
       >
@@ -77,32 +89,64 @@ onMounted(getOrderData);
       <el-col :span="3"
         ><div class="grid-content bg-purple">配達日時</div></el-col
       >
+      <el-col :span="2"
+        ><div class="grid-content bg-purple">支払方法</div></el-col
+      >
       <el-col :span="1"><!-- ここは余白--></el-col>
     </el-row>
-    <div v-for="currentUserData of currentOrderList">
-      <div v-for="(orderItem, i) of currentUserData.orderItemList">
+    <hr />
+    <div v-for="(currentUserData, i) of currentOrderList">
+      <div v-for="(orderItem, l) of currentUserData.orderItemList">
         <el-row :gutter="20">
           <el-col :span="1"> <!-- ここは余白--> </el-col>
-          <el-col :span="2">{{ i + 1 }}</el-col>
-          <el-col :span="3"><img :src="orderItem.item.imagePath" /></el-col>
+
+          <el-col :span="3"
+            ><RouterLink :to="'/itemDetail/' + orderItem.itemId">
+              <img class="food" :src="orderItem.item.imagePath" /> </RouterLink
+          ></el-col>
           <el-col :span="3">{{ orderItem.item.name }}</el-col>
           <el-col :span="3">{{ orderItem.size + "サイズ" }}</el-col>
           <el-col :span="3">{{ orderItem.quantity + "個" }}</el-col>
-          <el-col :span="3">{{ currentUserData.orderDate }}</el-col>
-          <el-col :span="3">{{ currentUserData.deliveryTime }}</el-col>
+          <el-col :span="3">{{
+            new Date(currentUserData.orderDate).toLocaleDateString()
+          }}</el-col>
+          <el-col :span="3">{{
+            new Date(currentUserData.deliveryTime).toLocaleString()
+          }}</el-col>
+          <el-col :span="2" v-if="currentUserData.paymentMethod === 1">{{
+            "現金支払"
+          }}</el-col>
+          <el-col :span="2" v-else>{{ "クレジット支払" }}</el-col>
           <el-col :span="1"><!-- ここは余白--></el-col>
         </el-row>
+        <hr />
       </div>
     </div>
   </div>
-  <div v-else>注文履歴がありません</div>
+  <div class="container" v-else>注文履歴がありません</div>
 </template>
 <style scoped>
+h1 {
+  padding-left: 50px;
+}
+.icon {
+  width: 100px;
+}
 bg-purple {
   background: #99a9bf;
 }
 .grid-content {
   border-radius: 4px;
   min-height: 36px;
+}
+.food {
+  width: 150px;
+  object-fit: cover;
+  border-radius: 20px;
+  cursor: pointer;
+}
+.food:hover {
+  opacity: 0.7;
+  color: white;
 }
 </style>
