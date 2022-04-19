33a976ed443c2ebd672d0493fb8685b8e2a51575
Mmy26 <yu.b1994masamura@gmail.com>
2022-04-19 10:33:13 +0900

    Jest

---
+++ src/__tests__/providers/useUserProvider.spec.ts
@@ -0,0 +1,13 @@
+import { User } from "../../types/User";
+import { useUserProvider } from "../../providers/useUserProvider";
+export {};
+// describe("providers/useUserProvider.ts",()=>{
+//   describe("setCurrentUserのテスト",()=>{
+//     test("引数のユーザーがstateのcurrentUserにセットされる",()=>{
+//       const expected = new User(0, "鈴木", "", "", "", "", "")
+//       jest.spyOn(useUserProvider,useUserProvider.setCurrentUser(expected))
+
+//       expect(useUserProvider().currentUser).toEqual(expected)
+//     })
+//   })
+// })
