9da8a600a3b1d893b74dd4858669285ce86ca037
grandorder999 <suzuo.kawabata@rakus-partners.co.jp>
2022-04-19 17:07:30 +0900

    ItemListのテスト（仮）

--- src/__tests__/views/ItemList.spec.ts
+++ src/__tests__/views/ItemList.spec.ts
@@ -4,15 +4,26 @@ import { mount } from "@vue/test-utils";
 import ElementPlus from "element-plus";
 
 describe.only("商品一覧画面のテスト", () => {
-  test("Mサイズの金額", () => {
+  test("inputタグのテスト", async () => {
     // ElementPlusもコンポーネントなので以下のように書く
     const wrapper = mount(ItemList, {
       global: {
         plugins: [ElementPlus],
       },
     });
-    const element = wrapper.find("#size");
-    console.log("elementの値:", element);
-    expect(element.text()).toBe("500");
+    const input = wrapper.find("input");
+    await input.setValue("ラーメン");
+    expect(input.element.value).toBe("ラーメン");
+  });
+
+  test("クリックイベントのテスト", async () => {
+    const wrapper = mount(ItemList, {
+      global: {
+        plugins: [ElementPlus],
+      },
+    });
+    await wrapper.get("button").trigger("click");
+    const elenment = wrapper.find("#item.name");
+    expect(elenment.text()).toBe("ラーメン");
   });
 });
