85093a05fbf416ff16a34932b79661a8d4854f19
grandorder999 <suzuo.kawabata@rakus-partners.co.jp>
2022-04-20 09:29:21 +0900

    画面遷移と同時にページ上部を表示する処理を追記

--- src/views/ItemDetail2.vue
+++ src/views/ItemDetail2.vue
@@ -87,7 +87,27 @@ const getToppingData = async (): Promise<void> => {
 };
 
 // 非同期で上記のメソッドを実行
-onMounted(getToppingData);
+onMounted(() => {
+  getToppingData();
+  // スクロールトップボタン
+  scrollTop(1); // 遅すぎるとガクガクになるので注意
+  function scrollTop(duration: number) {
+    let currentY = window.pageYOffset; // 現在のスクロール位置を取得
+    let step = duration / currentY > 1 ? 10 : 100; // 三項演算子
+    let timeStep = (duration / currentY) * step; // スクロール時間
+    let intervalId = setInterval(scrollUp, timeStep);
+    // timeStepの間隔でscrollUpを繰り返す。
+    // clearItervalのために返り値intervalIdを定義する。
+    function scrollUp() {
+      currentY = window.pageYOffset;
+      if (currentY === 0) {
+        clearInterval(intervalId); // ページ最上部に来たら終了
+      } else {
+        scrollBy(0, -step); // step分上へスクロール
+      }
+    }
+  }
+});
 
 /**
  小計金額の計算（変更され都度反映される）.
